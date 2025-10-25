69abb1992a7cf7864a948a5a7cea0dccec55af95
ThamizhCSE <thamizhanbanguna@gmail.com>
2025-10-25 14:26:36 +0530

    program complete

--- selection_sort.py
+++ selection_sort.py
@@ -1,1 +1,11 @@
 # created a python file directly from github
+a= [5,32,1,6,4]
+n= len(a)
+for i in range(0,n):
+    small=i
+    for j in range(i+1,n):
+        if a[small] > a[j]:
+            small = j
+    a[i],a[small] = a[small],a[i]
+
+print(a)
